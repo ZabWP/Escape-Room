4c641ed6d248e40662e7e71fc39cd25694691dee
ZabWP <zabdiel.bronola01@gmail.com>
2025-04-06 16:46:46 -0400

    aligned images

--- classroom.php
+++ classroom.php
@@ -6,7 +6,7 @@ $puzzle3_completed  = isset($_COOKIE['puzzle3_complete']) && $_COOKIE['puzzle3_c
 
 $inventory = isset($_COOKIE['inventory']) ? explode(',', $_COOKIE['inventory']) : [];
 
-$items = ['image1', 'image2', 'image3']; // Replace with your actual item IDs or filenames
+$items = ['backpack', 'laptop', 'phone']; // Replace with your actual item IDs or filenames
 
 if ($puzzle1_completed && !isset($_COOKIE['puzzle1_complete'])) {
     setcookie('puzzle1_complete', 'true', time() + 3600, '/');
@@ -26,31 +26,24 @@ if ($puzzle1_completed && !isset($_COOKIE['puzzle1_complete'])) {
         <div class="classroomContainer">
 
         
-        <div class="backpack">
-            <h3>Backpack</h3>
-            <?php foreach ($inventory as $item): ?>
-            <img src="images/<?= $item ?>.png" width="40">
-            <?php endforeach; ?>
-        </div>
-        
-        <?php if (!in_array('item1', $inventory)): ?>
-            <form method="POST" action="collect_item.php" style="position:absolute; top:150px; left:100px;" class="backpackContainer">
-                <input type="hidden" name="item" value="item1">
-                <input type="image" src="assets/backpack.png" class="backpack" alt="item1">
+   
+            <form method="POST" action="collect_item.php" class="backpackContainer" >
+                <input type="hidden" name="item" value="backpack">
+                <input type="image" src="assets/backpack.png" class="backpack" alt="backpack" style=" width: 20%; position: absolute; bottom: 0; right: 20%; cursor: default;">
             </form>
-        <?php endif; ?>
+      
 
-        <?php if (!in_array('item2', $inventory)): ?>
+        <?php if (!in_array('laptop', $inventory)): ?>
             <form method="POST" action="collect_item.php" style="position:absolute; top:300px; left:200px;" class="laptopContainer">
-                <input type="hidden" name="item" value="item2">
-                <input type="image" src="assets/laptop.png" class="laptop" alt="item2">
+                <input type="hidden" name="item" value="laptop">
+                <input type="image" src="assets/laptop.png" class="laptop" alt="laptop">
             </form>
         <?php endif; ?>
 
-        <?php if (!in_array('item3', $inventory)): ?>
+        <?php if (!in_array('phone', $inventory)): ?>
             <form method="POST" action="collect_item.php" style="position:absolute; top:400px; left:350px;" class="phoneContainer">
-                <input type="hidden" name="item" value="item3">
-                <input type="image" src="assets/phone.png" class="phone" alt="item3">
+                <input type="hidden" name="item" value="phone">
+                <input type="image" src="assets/phone.png" class="phone" alt="phone">
             </form>
         <?php endif; ?>
             </div>
