a44af7bade100bfbfb039f3f0189f4a8d69ffa68
ZabWP <zabdiel.bronola01@gmail.com>
2025-04-06 18:29:12 -0400

    repositioned images

--- classroom.php
+++ classroom.php
@@ -61,26 +61,34 @@ $items = ['backpack', 'laptop', 'phone'];
 
         <div class="classroomContainer">
 
-        <?php if (!in_array('backpack', $inventory)): ?>
-            <form method="POST" action="collectItem.php" class="backpackContainer" >
-                <input type="hidden" name="item" value="backpack">
-                <input type="image" src="assets/backpack.png" class="backpack" alt="backpack" style=" width: 20%; position: absolute; bottom: 0; right: 20%; cursor: default;">
-            </form>
-        <?php endif; ?>
+            <?php if (!in_array('backpack', $inventory)): ?>
+                <form method="POST" action="collectItem.php" class="backpackContainer" >
+                    <input type="hidden" name="item" value="backpack">
+                    <input type="image" src="assets/backpack.png" class="backpack" alt="backpack" style=" width: 31%; position: absolute; bottom: 0; right: 0%; cursor: default;">
+                </form>
+            <?php endif; ?>
 
-        <?php if (!in_array('laptop', $inventory)): ?>
-            <form method="POST" action="collectItem.php" style="position:absolute; top:300px; left:200px;" class="laptopContainer">
-                <input type="hidden" name="item" value="laptop">
-                <input type="image" src="assets/laptop.png" class="laptop" alt="laptop">
-            </form>
-        <?php endif; ?>
+            <?php if (!in_array('laptop', $inventory)): ?>
+                <form method="POST" action="collectItem.php" style="position:absolute; top:300px; left:200px;" class="laptopContainer">
+                    <input type="hidden" name="item" value="laptop">
+                    <input type="image" src="assets/laptop.png" class="laptop" alt="laptop">
+                </form>
+            <?php endif; ?>
 
-        <?php if (!in_array('phone', $inventory)): ?>
-            <form method="POST" action="collectItem.php" style="position:absolute; top:400px; left:350px;" class="phoneContainer">
-                <input type="hidden" name="item" value="phone">
-                <input type="image" src="assets/phone.png" class="phone" alt="phone">
-            </form>
-        <?php endif; ?>
+            <?php if (!in_array('phone', $inventory)): ?>
+                <form method="POST" action="collectItem.php" style="position:absolute; top:400px; left:350px;" class="phoneContainer">
+                    <input type="hidden" name="item" value="phone">
+                    <input type="image" src="assets/phone.png" class="phone" alt="phone">
+                </form>
+            <?php endif; ?>
+
+            <div class="rightExit">
+                
             </div>
+
+            <div class="leftExit">
+                
+                </div>
+        </div>
     </body>
 </html>
